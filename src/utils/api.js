856c6145bd8f2625a0d13eac1306ea68ccda5558
Angie <angie.taroga@gmail.com>
2025-02-18 16:35:58 -0500

    Added env variable

--- src/utils/api.js
+++
@@ -1,9 +0,0 @@
-export const API_HOST = "https://api.cloudinary.com/v1_1/doyhzunyk/auto/upload";
-export const API_HOST_TEST = "http://testing.gfassistance.com/upload/customers-file";
-
-export const UPLOAD_PRESET = "unsigned";
-
-export const API_KEY = "715483886979529";
-
-export const ENABLED_TYPES = ["csv", "png", "pdf"]
-
